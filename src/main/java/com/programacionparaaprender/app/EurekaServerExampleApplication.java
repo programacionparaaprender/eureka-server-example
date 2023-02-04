2fd7d0aac34233616d2f8d5c2bec97875092453e
lcorrea <yancel209@gmail.com>
2023-02-04 13:18:51 -0500

    cambios

--- src/main/java/com/programacionparaaprender/app/EurekaServerExampleApplication.java
+++ src/main/java/com/programacionparaaprender/app/EurekaServerExampleApplication.java
@@ -1,15 +1,22 @@
 package com.programacionparaaprender.app;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;  
 
 
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerExampleApplication {
-
-	public static void main(String[] args) {
+	 /* Get the class name to be printed on */  
+	   static Logger log = Logger.getLogger(EurekaServerExampleApplication.class.getName());  
+	public static void main(String[] args) throws IOException,SQLException{  
+	      log.debug("Hello this is a debug message");  
+	      log.info("Hello this is an info message");  
 		SpringApplication.run(EurekaServerExampleApplication.class, args);
 	}
 
